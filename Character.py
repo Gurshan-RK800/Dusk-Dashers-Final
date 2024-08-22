4b1c393c155365bd72195cda3d981aceddfba7d3
gurshan <gurshandhaliwal509@outlook.cpo>
2024-08-22 21:33:57 +0100

    Initial Push

---
+++ Character.py
@@ -0,0 +1,12 @@
+class Player:
+    def __init__(self, strength, health, agility):
+        self.strength = strength
+        self.health = health
+        self.agility = agility
+
+
+
+
+Leon = Player(70, 70, 60)
+Wesker = Player(80, 90, 40)
+Ethan = Player(60, 60, 90)
