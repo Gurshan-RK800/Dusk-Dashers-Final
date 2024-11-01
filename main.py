5d996db9d8c5db4aeddd60f72a4338fc164a418e
gurshandhaliwal <gurshandhaliwal509@outlook.com>
2024-11-01 10:44:54 +0000

    Movement of square

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import pygame
-from Character import Player
 
 pygame.init()
+screen = pygame.display.set_mode((1200, 700))
+player = pygame.Rect(600, 350, 50, 50)
+movement_speed = 2
 
-screen_width = 1200
-screen_height = 700
-
-screen = pygame.display.set_mode((screen_width, screen_height))
-while True:
+loop = True
+while loop:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: quit()
+        if event.type == pygame.QUIT:
+            loop = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player.y -= movement_speed
+    if keys[pygame.K_s]:
+        player.y += movement_speed
+    if keys[pygame.K_a]:
+        player.x -= movement_speed
+    if keys[pygame.K_d]:
+        player.x += movement_speed
+
+    screen.fill((255, 255, 255))
+    pygame.draw.rect(screen, (0, 0, 0), player)
+    pygame.display.update()
 
 pygame.quit()
